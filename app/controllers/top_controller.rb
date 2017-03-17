c1989d40f7a70029b517df4bce92e906f1f2be6d
Kohei Ikeda <kouhik06@gmail.com>
2017-03-17 06:58:42 +0000

    update files

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,7 @@
 class TopController < ApplicationController
   def index
+    if request.post? then
+      redirect_to tweets_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
   
   root "top#index"
+  post "/", to: "top#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
